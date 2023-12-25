2691ca1bac5ace11ce010a93aa7554e311a5fd0e
KojoAning <aningabankwa@gmail.com>
2023-12-25 06:56:54 +0000

    commit #4544

---
+++ env.py
@@ -0,0 +1,6 @@
+import os
+os.environ["DATABASE_URL"] = "postgres://dnjtknmg:xkhWiU7m8stNEPF0VGNNQO-xPZ4iby4K@mel.db.elephantsql.com/dnjtknmg"
+os.environ["SECRET_KEY"] = "b7461580-434e-4562-b677-4cb3a5d3c340"
+os.environ["CLOUDINARY_URL"] = "cloudinary://664456549386594:UzhklSgTj4-U16TxbmIEb0I0iQg@dnyizifid"
+os.environ["CLOUDINARY_SECRET"]="UzhklSgTj4-U16TxbmIEb0I0iQg"
+os.environ["CLOUDINARY_NAME"]="dnyizifid"
